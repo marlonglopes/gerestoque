fa472544bc94a5fd7e65ceaec25b2ed2fa53c078
Marlon Gomes Lopes <marlonglopes@gmail.com>
2011-02-24 15:59:46 -0300

    testes

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,5 +1,7 @@
 module ItemsHelper
 
+
+
 	def verif_mumps_icon(item)
 		if logged_in? and current_user.admin? 
 			item.parecer==item.parecer_mumps ? image_tag("icons/tick.png") : image_tag("icons/cross.png") 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,7 @@
 class Item < ActiveRecord::Base
 
-#	before_update :get_parecer_mumps
+	after_find :get_parecer_mumps
 	before_save :set_parecer_mumps
-#	after_find 	:get_mumps
-#	after_initialize :get_mumps
-
-#	include Consomewsghc
 
 	belongs_to :produto
 	belongs_to :marca
@@ -27,12 +23,6 @@ class Item < ActiveRecord::Base
 
 	def parecer_mumps
 		begin
-#		ret=ControllerConsomewsghc::wsghc(	:server=>"172.16.2.5",
-#														:uci=>"MWS",
-#														:volgrp=>"MWS",
-#														:rotina=>"^webServ",
-#														:id=>15,
-#														:param=>"2^#{self.produto_id}^#{self.marca_id}^#{self.parecer_id}")
 
 		ret=ControllerConsomewsghc::wsghc(	:id=>15,
 														:param=>"2^#{self.produto_id}^#{self.marca_id}^#{self.parecer_id}")
@@ -43,38 +33,12 @@ class Item < ActiveRecord::Base
 		end
 	end
 
-#	def parecer_id=(id)
-#    write_attribute(:parecer_id, id)
-#  end
-
-#  def parecer_id
-#	ret=wsghc(:id=>15,:param=>"2^#{self.produto_id}^#{self.parecer_id}")
-#	write_attribute(:parecer_id, ret)
-#	read_attribute(:parecer_id)
-##	self[:parecer_id]=ret
-##	read_attribute(:parecer_id)
-#  end
-
 private
 
-	def get_mumps2
-#		logger.info("####################### get_mumps")
-#		ret=ControllerConsomewsghc::wsghc(:id=>15,:param=>"2^#{self.produto_id}^#{self.parecer_id}")
-#		logger.info("####################### get_mumps  2^#{self.produto_id}^#{self.parecer_id}")
-#		self.parecer_id=ret
-#		logger.info("####################### get_mumps  #{ret.to_s}")
-#		puts ControllerConsomewsghc::wsghc(:id=>15,:param=>"2^#{self.produto_id}^#{self.parecer_id}")
-#		puts "after_find get_mumps"
-#		self.parecer_id=1
-	end
 	
 	def get_parecer_mumps
 		logger.info("####################### before_update :get_mumps")
-		ret=ControllerConsomewsghc::wsghc(	:server=>"172.16.2.5",
-														:uci=>"MWS",
-														:volgrp=>"MWS",
-														:rotina=>"^webServ",
-														:id=>15,
+		ret=ControllerConsomewsghc::wsghc(	:id=>15,
 														:param=>"2^#{self.produto_id}^#{self.marca_id}^#{self.parecer_id}")
 		self.parecer_id=ret
 	end
@@ -82,12 +46,6 @@ private
 
 	def set_parecer_mumps
 		logger.info("####################### before_save :set_mumps")
-#		ControllerConsomewsghc::wsghc(	:server=>"172.16.2.5",
-#													:uci=>"MWS",
-#													:volgrp=>"MWS",
-#													:rotina=>"^webServ",
-#													:id=>15,
-#													:param=>"1^#{self.produto_id}^#{self.marca_id}^#{self.parecer_id}")
 
 		ControllerConsomewsghc::wsghc(	:id=>15,
 													:param=>"1^#{self.produto_id}^#{self.marca_id}^#{self.parecer_id}")
